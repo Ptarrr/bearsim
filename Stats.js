ead9fea23c7ed38112185eb5fe54699194e6bf60
Ptarrr <ptarr@gmx.com>
2012-10-30 02:59:50 -0400

    Use my own, higher stats (iLvl 477)

--- Stats.js
+++ Stats.js
@@ -1,16 +1,16 @@
 // The statistics you gain from gear - as they appear in caster form
 var Stats = {
-   Armor:         15180,
-   Agility:       9939,
-   Intellect:     161,
-   Strength:      101,
-   Stamina:       12412,
-   CritRating:    3216,
-   HasteRating:   2126,
-   MasteryRating: 2314,
-   DodgeRating:   0,
-   HitRating:     1247,
-   ExpRating:     2134,
+   Armor:         17370,
+   Agility:       11277,
+   Intellect:     249,
+   Strength:      180,
+   Stamina:       17292,
+   CritRating:    6760,
+   HasteRating:   621+600,
+   MasteryRating: 3765,
+   DodgeRating:   600,
+   HitRating:     2560,
+   ExpRating:     3550-600, // WARNING: if this is over 3000, the stat value for 2k expertise will be wrong
    BonusArmor:    0,  // do NOT forget to also increase total armor if you increase this
 
    SD_Dodge:      0.45, // change this to 0.50 if you have 4pc T14
